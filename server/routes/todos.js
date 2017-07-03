c6f26f179212d5fa4e4f2a00e2db698a3ad11920
Mina S. Aziz <minaz922@gmail.com>
2017-07-03 23:35:49 +0200

    parse request body and create todo

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -10,9 +10,15 @@ router.get('/', async (req, res) => {
 
 
 router.post('/', (req, res) => {
-  const todoItem = res.body.todo;
-  const todo = Todo(todoItem);
-  todo.save();
+  const todoItem = req.body.todo;
+  const todo = new Todo(todoItem);
+  console.log(todoItem)
+  console.log(todo)
+  todo.save().then((result) => {
+    return res.json({data: result, error: null});
+  }).catch((err) => {
+    return res.json({data: {}, error: err});
+  });
 });
 
 export default router;
