4269b6086898f00bc4415cad654854700f83cbe6
Mina S. Aziz <minaz922@gmail.com>
2017-07-07 15:45:44 +0200

    load bootstrap styles

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
