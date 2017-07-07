220cf5304bd7296f24eb1d223cb4586866f483c4
Mina S. Aziz <minaz922@gmail.com>
2017-07-08 00:06:29 +0200

    prettify

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import {getTodosAction, addTodoAction, editTodoAction, deleteTodoAction} from './modules/todos';
+import {
+  getTodosAction,
+  addTodoAction,
+  editTodoAction,
+  deleteTodoAction,
+  changeViewModeAction
+} from './modules/todos';
 
 import TodoItem from './todoItem';
 
@@ -10,25 +16,39 @@ import './App.css';
 
 class App extends Component {
   componentDidMount() {
-    this.props.getTodosList();
+    this.props.getTodosList('ALL');
+  }
+
+  getTodos(ev) {
+    this.props.getTodosList(ev.target.value);
   }
 
   addTodo(ev) {
     ev.preventDefault();
-    const {title, desc, priority, date} = ev.target;
+    const { title, desc, priority, date } = ev.target;
 
     const newTodo = {
       name: title.value,
       description: desc.value,
       dueDate: date.value,
       priority: priority.value
-    }
+    };
 
     this.props.addTodo(newTodo);
   }
 
-  editTodo(guid, name, description, priority, dueDate, completed, completionDate) {
-    const newCompletionDate = completed ? (completionDate ? completionDate : new Date()) : null;
+  editTodo(
+    guid,
+    name,
+    description,
+    priority,
+    dueDate,
+    completed,
+    completionDate
+  ) {
+    const newCompletionDate = completed
+      ? completionDate ? completionDate : new Date()
+      : null;
     this.props.editTodo(guid, {
       name,
       description,
@@ -78,12 +98,77 @@ class App extends Component {
                 <option value="IMPORTANT">Important</option>
                 <option value="URGENT">Urgent</option>
               </select>
+            </div>
+          </div>
+          <div className="container">
+            <ul id="myUL">
+              {this.props.todos.map(todo =>
+                <TodoItem
+                  key={todo._id}
+                  guid={todo._id}
+                  title={todo.name}
+                  desc={todo.description}
+                  dueDate={todo.dueDate}
+                  priority={todo.priority}
+                  completed={todo.completed}
+                  editCb={this.editTodo.bind(this)}
+                  deleteCb={this.deleteTodo.bind(this)}
+                />
+              )}
+            </ul>
+            <div id="myDIV" className="header">
+              <h2>Add Todo</h2>
+              <form
+                className="add-todo-form"
+                onSubmit={this.addTodo.bind(this)}
+              >
+                <label htmlFor="title">Title:</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  id="title"
+                  defaultValue=""
+                />
 
-              <br />
+                <br />
 
-              <label></label>
-              <input type="submit" className="addBtn btn btn-success" value="Add" />
-            </form>
+                <label htmlFor="desc">Description:</label>
+                <textarea
+                  className="form-control"
+                  rows="4"
+                  name="desc"
+                  defaultValue="..."
+                />
+
+                <br />
+
+                <label htmlFor="date">Due Date:</label>
+                <input
+                  className="form-control"
+                  placeholder="Date"
+                  type="date"
+                  id="date"
+                />
+
+                <br />
+
+                <label htmlFor="priority">Priority:</label>
+                <select className="form-control" id="priority">
+                  <option value="NORMAL">Normal</option>
+                  <option value="IMPORTANT">Important</option>
+                  <option value="URGENT">Urgent</option>
+                </select>
+
+                <br />
+
+                <label />
+                <input
+                  type="submit"
+                  className="addBtn btn btn-success"
+                  value="Add"
+                />
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -91,19 +176,20 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     getTodosList: () => dispatch(getTodosAction()),
     addTodo: todo => dispatch(addTodoAction(todo)),
     editTodo: (guid, editedTodo) => dispatch(editTodoAction(guid, editedTodo)),
-    deleteTodo: guid => dispatch(deleteTodoAction(guid))
-  }
-}
+    deleteTodo: guid => dispatch(deleteTodoAction(guid)),
+    // changeViewMode: viewMode => dispatch(changeViewModeAction(viewMode))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
