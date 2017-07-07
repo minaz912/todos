a935d30f7175493c227a5bc4afe343955d84e780
Mina S. Aziz <minaz922@gmail.com>
2017-07-07 17:28:12 +0200

    remove log statement

--- client/src/modules/todos.js
+++ client/src/modules/todos.js
@@ -3,7 +3,6 @@ export const getTodosAction = (dispatch) => {
   return (dispatch) => {
     fetch('/todos')
             .then((response) => {
-              console.log('hello');
                 if (!response.ok) {
                     throw Error(response.statusText);
                 }
