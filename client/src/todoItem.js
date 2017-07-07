33091038fe74c4ae4f771d549649c78adbac5ccb
Mina S. Aziz <minaz922@gmail.com>
2017-07-07 15:45:20 +0200

    create stateless component for todoItem

---
+++ client/src/todoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+  const priorityClsName = `icon-right ${props.priority}`;
+  return (
+    <li className='todo-container'>
+      <div className="todo-name">
+        {props.name}
+      </div>
+      <div>
+        <span className={priorityClsName}>
+          {props.priority.toLowerCase()}
+        </span>
+        <span className='icon-right icon-close'>
+          x
+        </span>
+      </div>
+    </li>
+  )
+}
+
+export default TodoItem;
