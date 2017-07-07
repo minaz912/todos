9ae683b0a5606b6a10016fa4bc3ba93c51e10c4e
Mina S. Aziz <minaz922@gmail.com>
2017-07-07 21:04:27 +0200

    show todo descriptions and add more styling

--- client/src/todoItem.js
+++ client/src/todoItem.js
@@ -19,19 +19,24 @@ const TodoItem = (props) => {
 
   return (
     <li className={containerClsName}>
-      <div className="todo-name">
-        {props.title || '<Untitled Todo>'}
+      <div className="todo-row todo-row-first">
+        <div className="todo-name">
+          {props.title || '<Untitled Todo>'}
+        </div>
+        <div>
+          <span className={priorityClsName}>
+            {props.priority.toLowerCase()}
+          </span>
+          <span className='icon-right icon-close' onClick={props.editCb.bind(this, props.guid, props.title, props.desc, props.priority, props.dueDate, !props.completed, props.completionDate)}>
+            &#9745;
+          </span>
+          <span onClick={props.deleteCb.bind(this, props.guid)} className='icon-right icon-close'>
+            x
+          </span>
+        </div>
       </div>
-      <div>
-        <span className={priorityClsName}>
-          {props.priority.toLowerCase()}
-        </span>
-        <span className='icon-right icon-close' onClick={props.editCb.bind(this, props.guid, props.title, props.desc, props.priority, props.dueDate, !props.completed, props.completionDate)}>
-          &#9745;
-        </span>
-        <span onClick={props.deleteCb.bind(this, props.guid)} className='icon-right icon-close'>
-          x
-        </span>
+      <div className="todo-row todo-row-second">
+        {props.desc}
       </div>
     </li>
   )
