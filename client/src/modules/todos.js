1c5e93a89a7acd46fe7cdbb7b65ec4d1f5e08a27
Mina S. Aziz <minaz922@gmail.com>
2017-07-07 14:17:00 +0200

    create action to fetch todos from server

--- client/src/modules/todos.js
+++ client/src/modules/todos.js
@@ -1,7 +1,34 @@
+// ACTIONS
+export const getTodosAction = (dispatch) => {
+  return (dispatch) => {
+    fetch('/todos')
+            .then((response) => {
+              console.log('hello');
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then((response) => response.json())
+            .then((todos) => dispatch(getTodosListSuccess(todos.data)));
+  }
+}
+
+export const getTodosListSuccess = (todos) => {
+  return {
+    type: 'FETCH_TODOS_LIST_SUCCESS',
+    todos
+  }
+}
+
+
+// REDUCERS
 const initialState = []
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'FETCH_TODOS_LIST_SUCCESS':
+      return action.todos;
     default:
       return state
   }
